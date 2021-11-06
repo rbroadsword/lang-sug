0deb627a8bea7436a711746c87196a97dfed6a77
Ryan Broadsword <rbroadsword@gmail.com>
2021-11-05 22:42:35 -0700

    add js const for drag name and result from drag mom question, make branch for value of result to drag mom.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-
+  $("#form-one").submit(function(event) {
+    event.preventDefault();
+    const question1 = $("input#drag-name").val();
+    const result = parseInt($("#drag-mom").val());
+    
+    if (result === 1) {
+      $(".drag-name").text(question1);
+      $("#lang-2").toggle();
+      $("#main").hide();
+    } else if (result === 2) {
+      $(".drag-name").text(question1);
+      $("#lang-3").toggle();
+      $("#main").hide();
+    } else (result === 3) {
+      $(".drag-name").text(question1);
+      $("#lang-1").toggle();
+      $("#main").hide();
+    }
+  });
 });
