cec16f709af380398c3c93e67f9dd4360f51f4e8
Ryan Broadsword <rbroadsword@gmail.com>
2021-11-07 17:17:21 -0800

    add new dragStyle var, change result var.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
   $("#form-one").submit(function(event) {
     event.preventDefault();
-    const question1 = $("input#drag-name").val();
-    const result = parseInt($("#drag-mom").val());
-    
-    if (result === 1) {
-      $(".drag-name").text(question1);
+    const dragName = $("input#drag-name").val();
+    const dragMom = parseInt($("#drag-mom").val());
+    const dragStyle = parseInt($('#drag-style').val());
+    const result = dragMom + dragStyle;
+
+    if (result <= 13) {
+      $(".drag-name").text(dragName);
       $("#lang-2").toggle();
       $("#main").hide();
-    } else if (result === 2) {
-      $(".drag-name").text(question1);
+    } else if (result >= 14 && result <= 23) {
+      $(".drag-name").text(dragName);
       $("#lang-3").toggle();
       $("#main").hide();
-    } else {
-      $(".drag-name").text(question1);
+    } else if (result >= 30) {
+      $(".drag-name").text(dragName);
       $("#lang-1").toggle();
       $("#main").hide();
     }
